817c73d0b6455f75c9c99d276cfe06e7b0cd6a4e
8BitBlimp <63862081+8BitBlimp@users.noreply.github.com>
2021-08-19 17:45:48 +0200

    intro command working

---
+++ commands/introductions/Teo.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+  return message.channel.send(`Coming... probably`);
+
+};
+
+module.exports.help = {
+  name: "teo"
+};
