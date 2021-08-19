288013c9c8607d4985848cdaab2bbfa8b8452572
8BitBlimp <63862081+8BitBlimp@users.noreply.github.com>
2021-08-19 17:18:27 +0200

    Update Nicolas.js

---
+++ commands/introductions/Nicolas.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+  return message.channel.send(`Short introduction: The creator of this god awful bot. please help me!!!!`);
+
+};
+
+module.exports.help = {
+  name: "nicolas"
+};
