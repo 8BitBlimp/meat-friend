d0af24351c7291c214b1b7a92aaa1e8de41b1c1c
8BitBlimp <63862081+8BitBlimp@users.noreply.github.com>
2021-08-21 18:07:16 +0200

    idek anymore

---
+++ commands/help.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+    let embed = new Discord.MessageEmbed()
+        .setColor('RANDOM')
+        .setTitle('Commands')
+        .addField('Introduction Commands:', 'Adrienne', 'Amber', 'Emma', 'Magnus', 'Michael', 'Molly', 'Nicolas', 'Rebecca', 'Teo', 'Valerie')
+        .addField('Mod Commands:', 'Ban', 'Kick')
+        .addField('Misc commands:', 'Avatar', 'Help', 'Kiss', 'Kissultra', 'Ping', 'Report')
+        .setFooter(target.user.displayAvatarURL);
+
+    message.channel.send(embed);
+  }
+
+  module.exports.help = {
+    name: "help"
+  }
