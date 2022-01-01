ed2fd057073f475de61e8de5fbe635ecf54f954e
8BitBlimp <63862081+8BitBlimp@users.noreply.github.com>
2022-01-01 16:38:42 +0100

    hopefully fixed nekopat

---
+++ commands/nekoPat.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const client = require('nekos.life');
+const neko = new client();
+
+module.exports.run = async (bot, message, args) => {
+    message.channel.send(neko.sfw.pat())
+  }
+
+  module.exports.help = {
+    name: "nekoPat"
+  }
