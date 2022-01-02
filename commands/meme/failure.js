820bfcc24682c2e8e52c83869645f1f44940edee
8BitBlimp <63862081+8BitBlimp@users.noreply.github.com>
2022-01-02 13:49:18 +0100

    Update failure.js

--- commands/meme/failure.js
+++ commands/meme/failure.js
@@ -1,4 +1,6 @@
-
+const Meme = require('memer-api');
+const memer = new Meme('8gzAiAfiDsh');
+const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
     const user = message.mentions.users.first();
