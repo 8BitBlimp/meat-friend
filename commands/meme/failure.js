0796046deccc3c7865914c7f44c1fcafadfa3dc5
8BitBlimp <63862081+8BitBlimp@users.noreply.github.com>
2022-01-02 14:36:25 +0100

    Update failure.js

--- commands/meme/failure.js
+++ commands/meme/failure.js
@@ -6,13 +6,13 @@ const fetch = require('node-fetch');
 module.exports.run = async (bot, message, args) => {
     const user = message.mentions.users.first();
     try{
-        fetch(`https://memer-api.live/api/v4/failure?token=MemerAPIToken&avatars=${user.displayAvatarURL()}`)
+        fetch(`https://memer-api.live/api/v4/failure?token=8gzAiAfiDsh&avatars=${user.displayAvatarURL()}`)
         .then(res => res.buffer())
         .then(data => message.channel.send(data))
     }
     catch(error) {
-        fetch(`https://memer-api.live/api/v4/failure?token=MemerAPIToken&avatars=${message.author.displayAvatarURL()}`)
-        .then(res => res.buffer())
+        fetch(`https://memer-api.live/api/v4/failure?token=8gzAiAfiDsh&avatars=${message.author.displayAvatarURL()}`)
+        .then(res => res.bffer())
         .then(data => message.channel.send(data))
     }
     
