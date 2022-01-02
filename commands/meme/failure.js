9d13cc2fa23485055ae2dca46db2875ed713357f
8BitBlimp <63862081+8BitBlimp@users.noreply.github.com>
2022-01-02 14:56:02 +0100

    Update failure.js

--- commands/meme/failure.js
+++ commands/meme/failure.js
@@ -8,13 +8,13 @@ module.exports.run = async (bot, message, args) => {
     try{
         memer.failure(user.displayAvatarURL()).then(image => {
             var attachment = new Discord.MessageAttachment(image, "failure.png");
-            messsage.channel.send(attachment)
+            message.channel.send(attachment)
         })
     }
     catch(error) {
         memer.failure(message.author.displayAvatarURL()).then(image => {
             var attachment = new Discord.MessageAttachment(image, "failure.png");
-            messsage.channel.send(attachment)
+            message.channel.send(attachment)
         })
     }
 }
