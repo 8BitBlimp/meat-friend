9d1058927c17584e7ca5c4a53a5c74bf6579a65d
8BitBlimp <63862081+8BitBlimp@users.noreply.github.com>
2022-01-06 15:43:44 +0100

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -4,9 +4,11 @@ module.exports.run = async (bot, message, args) => {
     if(message.author.id == '605442511997108224') {
         colour = args[0];
         
-        let role = message.guild.roles.cache.find(role => role.name === "Professional Campers");
-        role.setColor(colour)
-        
+        fuck = message.guildId()
+
+        client.guilds.cache.forEach((g) => { const role = guild.roles.cache.find((r) => r.name === 'Professional Campers'); })
+        role.setColor('#F0F768')
+
     }
     else{
         message.channel.send(`you don't have permission to do this.`)
