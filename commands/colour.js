e59897b2c131cb5ffdcda5d101a9ce286c377e38
HollowHuu <nico@mmx.dk>
2022-01-07 11:21:51 +0100

    Update colour.js

--- commands/colour.js
+++ commands/colour.js
@@ -1,11 +1,10 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
-    if(message.author.id == args[0]) {
-        colour = args[1];
+    if(message.author.id == '605442511997108224') {
         
-        let role = message.guild.roles.cache.find(r => r.id === "827907813699092511");
-        role.setColor(args[0]);
+        let role = message.guild.roles.cache.find(r => r.id === args[0]);
+        role.setColor(args[1]);
 
     }
     else{
