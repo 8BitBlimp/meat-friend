d544b13433e227b153f27f838800ae7e09f7a9b2
HollowHuu <nico@mmx.dk>
2022-01-17 11:50:12 +0100

    Update shutdown.js

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -1,16 +1,19 @@
 const Discord = require('discord.js');
+const owners = ['605442511997108224', '368081723344420866'];
 
 module.exports.run = async (bot, message, args) => {
-    if(message.author.id == '605442511997108224') {
-        print('Shutting down')
-        message.channel.send('Shutting down')
-        Discord.Shard.kill(Discord.Shard.id)
-    }
-    else{
-        message.channel.send(`you don't have permission to do this.`)
-    }
+    
+  if(owners.includes(message,author.id())) {
+    print('Shutting down')
+    message.channel.send('Shutting down')
+    Discord.Shard.kill(Discord.Shard.id)
   }
+
+  else{
+    message.channel.send(`you don't have permission to do this.`)
+  }
+}
 
   module.exports.help = {
     name: "shutdown"
-  }
+}
