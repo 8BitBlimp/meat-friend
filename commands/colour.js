12f6b83ed7e29f4d8fc2a7a65fd47433cda274cb
8BitBlimp <63862081+8BitBlimp@users.noreply.github.com>
2022-02-10 23:58:22 +0100

    Update colour.js

--- commands/colour.js
+++ commands/colour.js
@@ -2,17 +2,18 @@ const Discord = require('discord.js');
 const config = require('./utility-config.json')
 
 module.exports.run = async (bot, message, args) => {
-  if(message.member.hasPermission('ADMINISTRATOR')) {
-        colour = arguments[1];
+  if(message.author.id == '605442511997108224') {
+    colour = args[0]
+    haha = args[1]
         
-        let role = message.guild.roles.cache.find(r => r.id === arguments[0]);
-        role.setColor(colour);
+    let role = message.guild.roles.cache.find(r => r.id === "941456234182225951");
+    role.setColor(args[0]);
 
     }
-    else{
-        message.channel.send(`you don't have permission to do this.`)
-    }
+  else{
+    message.channel.send(`you don't have permission to do this.`)
   }
+}
 
   module.exports.help = {
     name: "colour"
