1bddaaa1127ff51dc0df2d8ee03d8fa5ff9b8930
8BitBlimp <63862081+8BitBlimp@users.noreply.github.com>
2022-02-10 23:20:50 +0100

    Update colour.js

--- commands/colour.js
+++ commands/colour.js
@@ -3,9 +3,9 @@ const config = require('./utility-config.json')
 
 module.exports.run = async (bot, message, args) => {
   if(message.member.hasPermission('ADMINISTRATOR')) {
-        colour = args[1];
+        colour = args[2];
         
-        let role = message.guild.roles.cache.find(r => r.id === args[0]);
+        let role = message.guild.roles.cache.find(r => r.id === args[1]);
         role.setColor(colour);
 
     }
